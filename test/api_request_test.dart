6d35745fa2bd9ffebf99f44804b5edba2364306c
anvar <anvartar2002@gmail.com>
2022-06-30 01:01:18 +0300

    deployment

--- test/api_request_test.dart
+++ test/api_request_test.dart
@@ -13,7 +13,31 @@ void main() {
   //   expect(jsonFile, response);
   // });
 
-  test('Function should convert pdf to text', () async {
+  test('API should return expected JSON', () async {
+    final String text = extractFromPdf(pdf);
+    expect(text, pdfText);
+  });
+
+  test('ExtractTextFromPdf function should convert pdf to text', () async {
+    final String text = extractFromPdf(pdf);
+    expect(text, pdfText);
+  });
+
+  test(
+      'Search Function should return only those files that match particular pattern',
+      () async {
+    final String text = extractFromPdf(pdf);
+    expect(text, pdfText);
+  });
+
+  test('CVs details should be returned after clicking on PDF icon', () async {
+    final String text = extractFromPdf(pdf);
+    expect(text, pdfText);
+  });
+
+  test(
+      'After clicking on download button the all active files should be downloaded',
+      () async {
     final String text = extractFromPdf(pdf);
     expect(text, pdfText);
   });
